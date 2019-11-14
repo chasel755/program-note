3e01e8de8051a0703cf4f3fee0198f8c210ac38a
ChaselXu <xuchaozhou@idouzi.com>
2019-11-14 14:30:21 +0800

    岛屿数量

--- go/data-struct/numIslands.go
+++ go/data-struct/numIslands.go
@@ -1,5 +1,25 @@
 package data-struct
 
+/**
+岛屿数量
+给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
+
+示例 1:
+输入:
+11110
+11010
+11000
+00000
+输出: 1
+
+示例 2:
+输入:
+11000
+11000
+00100
+00011
+输出: 3
+*/
 func numIslands(grid [][]byte) int {
     count := 0
     width := 0
@@ -9,14 +29,14 @@ func numIslands(grid [][]byte) int {
     }
     for j:=0; j<height; j++ {
         for i:=0; i<width; i++ {
-            count += BreadthFirstSearch(grid, j, i, height, width)
+            count += breadthFirstSearchNumIslands(grid, j, i, height, width)
         }
     }
     return count
 }
 
 /*深度优先搜索*/
-func DepthFirstSearch(grid [][]byte, j int, i int, height int, width int) int {
+func depthFirstSearchNumIslands(grid [][]byte, j int, i int, height int, width int) int {
     if j<0 || j >= height || i<0 || i>=width {
         return 0
     }
@@ -32,7 +52,7 @@ func DepthFirstSearch(grid [][]byte, j int, i int, height int, width int) int {
 }
 
 /*广度优先搜索*/
-func BreadthFirstSearch(grid [][]byte, j int, i int, height int, width int) int {
+func breadthFirstSearchNumIslands(grid [][]byte, j int, i int, height int, width int) int {
     if j<0 || j >= height || i<0 || i>=width {
         return 0
     }
